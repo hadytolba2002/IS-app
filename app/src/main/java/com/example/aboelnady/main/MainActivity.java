7aad539d610bc8e5166788ae05ea83a787d8e0a8
hadytolba2002 <hs1874@fayoum.edu.eg>
2024-03-18 21:39:05 +0200

    first commit

---
+++ app/src/main/java/com/example/aboelnady/main/MainActivity.java
@@ -0,0 +1,81 @@
+
+package com.example.aboelnady.main;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import com.example.aboelnady.R;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements MainView {
+
+    private RecyclerView recyclerView;
+    private ProgressBar progressBar;
+    private MainPresenter presenter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        recyclerView = findViewById(R.id.list);
+        progressBar = findViewById(R.id.progress);
+        presenter = new MainPresenter(this, new FindItemsInteractor());
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        presenter.onResume();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        presenter.onDestroy();
+        super.onDestroy();
+    }
+
+    @Override
+    public void showProgress() {
+        progressBar.setVisibility(View.VISIBLE);
+        recyclerView.setVisibility(View.INVISIBLE);
+    }
+
+    @Override
+    public void hideProgress() {
+        progressBar.setVisibility(View.INVISIBLE);
+        recyclerView.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void setItems(List<String> items) {
+        recyclerView.setAdapter(new MainAdapter(items, presenter::onItemClicked));
+    }
+
+    @Override
+    public void showMessage(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+    }
+}
